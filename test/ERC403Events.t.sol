513e81818b99644e062944a434ab685f6f2c9d1e
eugenioclrc <eugenioclrc@gmail.com>
2024-07-21 07:29:38 -0300

    add extra tests

--- test/ERC403Events.t.sol
+++ test/ERC403Events.t.sol
@@ -50,4 +50,46 @@ contract ERC1155EventsTest is Test {
         vm.prank(bob);
         erc20.transfer(alice, 1 ether - 1);
     }
+
+    function testTransferOneAssert() public {
+        token.mint(bob, 1337, 10, "");
+        vm.label(address(token.tokenIdToERC20(1337)), "erc20");
+
+        ERC20 erc20 = ERC20(token.tokenIdToERC20(1337));
+
+
+        vm.expectEmit(true, true, true, true);
+        emit ERC403.TransferSingle(address(erc20), bob, alice, 1337, 2);
+        vm.expectEmit(true, true, true, true);
+        emit ERC20.Transfer(bob, alice, 2 ether);
+
+        vm.prank(bob);
+        erc20.transfer(alice, 2 ether);
+
+        address user = makeAddr("user");
+
+        vm.expectEmit(true, true, true, true);
+        emit ERC403.TransferSingle(address(erc20), alice, address(0),1337, 1);
+        vm.expectEmit(true, true, true, true);
+        emit ERC20.Transfer(alice, user, 0.5 ether);
+
+        vm.prank(alice);
+        erc20.transfer(user, 0.5 ether);
+
+        vm.expectEmit(true, true, true, true);
+        emit ERC403.TransferSingle(address(erc20), bob, user,1337, 1);
+        vm.expectEmit(true, true, true, true);
+        emit ERC20.Transfer(bob, user, 0.5 ether);
+
+        vm.prank(bob);
+        erc20.transfer(user, 0.5 ether);
+
+
+        vm.expectEmit(true, true, true, true);
+        emit ERC20.Transfer(alice, address(0xbeef), 0.5 ether);
+
+        vm.prank(alice);
+        erc20.transfer(address(0xbeef), 0.5 ether);
+
+    }
 }
